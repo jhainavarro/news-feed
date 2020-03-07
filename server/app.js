a5e7fb4cdf804169496f79f88da16f49713a1d9b
Jhai Navarro <jhainavarro@gmail.com>
2020-03-08 03:23:31 +0800

    Add CORS middleware

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ import feedRouter from './routes/feed';
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +18,5 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/sources', sourcesRouter);
 app.use('/feed', feedRouter);
-
 
 export default app;
