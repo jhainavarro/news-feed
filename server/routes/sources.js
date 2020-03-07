3596b6a76b22c436597e6e7ce195e5b00aef65a0
Jhai Navarro <jhainavarro@gmail.com>
2020-03-08 03:14:08 +0800

    Call newsapi.org for list of sources

--- server/routes/sources.js
+++ server/routes/sources.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import NewsApi from 'newsapi';
 
 const router = express.Router();
 
+const newsApi = new NewsApi('0b76fba0aae94ffc8fad83f24d14237f');
+
 router.get('/', (req, res, next) => {
-  res.send('sources list');
+  newsApi.v2.sources({ language: 'en' })
+    .then(result => {
+        res.send(result);
+    })
+    .catch(() => {
+        res.status(500).send('Unable to fetch list of sources');
+    });
 });
 
 export default router;
